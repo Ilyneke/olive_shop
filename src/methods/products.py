c7d848c5ceb4a08b427b06133824fbc507e83169
dmitry <kharisov.dmitry101@gmail.com>
2024-06-21 00:57:40 +0500

    image fix

--- src/methods/products.py
+++ src/methods/products.py
@@ -5,6 +5,13 @@ from querysets.products import get_products_qs
 from settings.base import API_DOMAIN
 
 
+def get_image_url(image: str | None) -> str:
+    if image is not None:
+        return image.replace('/code', f'{API_DOMAIN}/olive')
+    else:
+        return 'https://kharisov.space/olive/storage/no-image-available-icon-vector.jpg'
+
+
 async def get_products_method(session: AsyncSession) -> list:
     smtm = await session.execute(get_products_qs())
     products = smtm.scalars().all()
@@ -17,7 +24,6 @@ async def get_products_method(session: AsyncSession) -> list:
             'discount': float(product.discount),
             'discounted_price': float(product.price - product.price * product.discount / 100),
             'variety': product.variety.variety,
-            'image': product.image.replace('/code', f'{API_DOMAIN}/olive') if product.image is not None
-            else 'https://kharisov.space/olive/storage/no-image-available-icon-vector.jpg'
+            'image': get_image_url(product.image),
         } for product in sorted(products, key=lambda x: x.variety.variety)
     ]

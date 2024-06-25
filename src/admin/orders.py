4accd9c8a840c906e065d6610b8710c631c0735c
dmitry <kharisov.dmitry101@gmail.com>
2024-06-25 22:00:12 +0500

    save orders

--- src/admin/orders.py
+++ src/admin/orders.py
@@ -6,9 +6,10 @@ from models import Orders
 class OrdersAdmin(ModelView, model=Orders):  # type: ignore
     name_plural = 'orders'
     name = 'order'
-    icon = 'fa-solid fa-cart-shopping'
+    icon = 'fa-solid fa-credit-card'
     column_list = [
         Orders.total_sum,
+        Orders.currency,
         Orders.phone,
         Orders.email,
     ]
@@ -16,5 +17,6 @@ class OrdersAdmin(ModelView, model=Orders):  # type: ignore
         Orders.created_at,
         Orders.modified_at,
     ]
+    column_formatters = {Orders.total_sum: lambda x: x / 100}
     page_size = 25
     page_size_options = [50, 100]
